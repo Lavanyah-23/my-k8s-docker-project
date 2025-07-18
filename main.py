7559519de134f0394eb0bd0f599d4ac6b3fdd542
Lavanya Peddibotla <43400337-venkatalalithal@users.noreply.replit.com>
2025-07-18 16:16:15 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
-app = Flask('app')
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    print(request.headers)
-    return render_template(
-        'index.html',
-        user_id=request.headers['X-Replit-User-Id'],
-        user_name=request.headers['X-Replit-User-Name'],
-        user_roles=request.headers['X-Replit-User-Roles'],
-        user_bio=request.headers['X-Replit-User-Bio'],
-        user_profile_image=request.headers['X-Replit-User-Profile-Image'],
-        user_teams=request.headers['X-Replit-User-Teams'],
-        user_url=request.headers['X-Replit-User-Url']
-    )
+def hello():
+    return "Hello from Kubernetes and Docker!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
-
+app.run(host='0.0.0.0', port=5000)
